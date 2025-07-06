431606d2552f7c996e1e8bed674c8001128bd06a
skiee9 <chaudharyayushi785@gmail.com>
2025-07-06 22:26:54 +0530

    users route setup

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -9,7 +9,7 @@ router.post("/login", login)
 router.post("/logout", logout)
 
 router.post("/onboarding", protectRoute, onboard);
-// router.get("/me", protectRoute, (req, res) => {
-//   res.status(200).json({ success: true, user: req.user });
-// });
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 export default router;
