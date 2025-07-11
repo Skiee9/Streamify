89eb2da703ace3df7aa540deccc6e769ce955864
skiee9 <chaudharyayushi785@gmail.com>
2025-07-11 21:09:26 +0530

    friendrequest handling for the sender and recipient

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 export async function getRecommendedUsers(req, res){
 
@@ -34,4 +35,56 @@ export async function getMyFriends (req, res){
 
     }
 
+}
+
+
+export async function sendFriendRequest(req, res) {
+    try{
+   const myId=req.user.id;
+   const{id : recipientId}= req.params;
+
+   //prevetn senfding req to yourself
+   if(myId===recipientId){
+    return res.status(400).json({message:"you cannot send friends request to yourself"})
+   }
+
+
+  const recipient = await User.findById(recipientId)
+   if(!recipient){
+    return res.status(404).json({message:"User not found"})
+   }
+
+
+
+   //check if user is already friends
+   if (recipient.friends.includes(myId)){
+    return res.status(400).json({message:"you are already friends with the user"})
+   }
+    
+
+   //check if friends req already exists
+   const existingRequest= await FriendRequest.findOne({
+    $or:[
+        {sender: myId, recipient: recipientId},
+        {sender: recipientId, recipient: myId}
+    ]
+   })
+
+
+   //if request exists, return status 400
+   if (existingRequest){
+    return res.status(400).json({message:"Friend request already exists"})
+   }
+
+    //create new friend request
+    const friendRequest = await FriendRequest.create({
+        sender: myId,
+        recipient: recipientId
+    });
+    }
+    
+    catch(error){
+
+    }
+    
 }
