a6280f46f2e2aacf02effa54e45544d776d15232
skiee9 <chaudharyayushi785@gmail.com>
2025-07-14 13:02:47 +0530

    stream token for chat and video calls

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUsers,
+     getMyFriends,
+      sendFriendRequest,
+       acceptFriendRequest, 
+       getFriendRequests,
+        getOutgoingFriendReqs,
+     } from "../controllers/user.controller.js";
 
  const router = express.Router();
 
@@ -13,9 +19,10 @@ router.use(protectRoute)
 
 
  router.get("/",getRecommendedUsers);
-
-
-
  router.get("/friends", getMyFriends);
+ router.post("friend-request/:id", sendFriendRequest);
+ router.put("/friend-request/:id/accept",acceptFriendRequest);
+ router.get("/friend-requests",getFriendRequests);
+ router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
  export default router;
