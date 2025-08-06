75c164ae62ee4796c9b33e3fa28c027a2f997a24
skiee9 <chaudharyayushi785@gmail.com>
2025-08-06 23:50:57 +0530

    installed react hot toast and use it for the toast of hy user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,26 @@ import ChatPage from './pages/ChatPage'
 import CallPage from './pages/CallPage'
 import NotificationsPage from './pages/NotificationsPage'
 import SignUpPage from './pages/SignUpPage'
+import OnboardingPage from './pages/OnboardingPage'
+import LoginPage from './pages/LoginPage'
+import toast, { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className='bg-red-500 h-screen' data-theme="coffee">
-  
+  <button onClick={()=> toast.success('Hello lady!')}>Click me</button>
 <Routes>
 <Route path="/" element={<HomePage/>} />
+<Route path="/login" element={<LoginPage/>}/>
 <Route path="/chat" element={<ChatPage/>} />
 <Route path="/call" element={<CallPage/>} />
 <Route path="/notifications" element={<NotificationsPage/>} />
-<Routes path="/signup" element={<SignUpPage/>}/>
+<Route path="/signup" element={<SignUpPage/>} />
+<Route path="/onboarding" element={<OnboardingPage/>}/>
 
 </Routes>
+
+<Toaster/>
     </div>
   )
 }
