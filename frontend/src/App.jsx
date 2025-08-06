dae128957eefa69a03a15b4d462d91a7b0e1e8a3
skiee9 <chaudharyayushi785@gmail.com>
2025-08-06 23:35:35 +0530

    added the page folder and files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage'
+import ChatPage from './pages/ChatPage'
+import CallPage from './pages/CallPage'
+import NotificationsPage from './pages/NotificationsPage'
+import SignUpPage from './pages/SignUpPage'
 
 const App = () => {
   return (
-    <div className='bg-red-500 h-screen'>
-    APP
-      
+    <div className='bg-red-500 h-screen' data-theme="coffee">
+  
+<Routes>
+<Route path="/" element={<HomePage/>} />
+<Route path="/chat" element={<ChatPage/>} />
+<Route path="/call" element={<CallPage/>} />
+<Route path="/notifications" element={<NotificationsPage/>} />
+<Routes path="/signup" element={<SignUpPage/>}/>
+
+</Routes>
     </div>
   )
 }
