e92942d257fcf01c14bb21dc437fb592ec5f2fd2
skiee9 <chaudharyayushi785@gmail.com>
2025-08-08 18:33:44 +0530

    adding the private routes to the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Navigate } from 'react-router'
 import HomePage from './pages/HomePage'
 import ChatPage from './pages/ChatPage'
 import CallPage from './pages/CallPage'
@@ -13,8 +13,16 @@ import { axiosInstance } from './lib/axios.js'
 
 const App = () => {
   //tanstack query
-const {data}= useQuery
+const {data:authData, isLoading,error}= useQuery({
+  queryKey:["authUser"],
+  queryFn: async () => {
+    const response = await axiosInstance.get("/auth/me")
+    return res.data
+  },
+  retry: false, // Disable retry on failure
+})
 
+const authUser = authData?.user || null;
  
 
 
@@ -26,13 +34,13 @@ return (
     <div className='h-screen ' data-theme="coffee">
 
 <Routes>
-<Route path="/" element={<HomePage/>} />
-<Route path="/login" element={<LoginPage/>}/>
-<Route path="/chat" element={<ChatPage/>} />
-<Route path="/call" element={<CallPage/>} />
-<Route path="/notifications" element={<NotificationsPage/>} />
-<Route path="/signup" element={<SignUpPage/>} />
-<Route path="/onboarding" element={<OnboardingPage/>}/>
+<Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login" />} />
+<Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>} />
+<Route path="/login" element={!authUser ? <LoginPage/>: <Navigate to="/"/>}/>
+<Route path="/notifications" element={authUser ? <NotificationsPage/> : <Navigate to="/login"/>} />
+<Route path="/chat" element={ authUser ? <ChatPage/>: <Navigate to="/login"/>} />
+<Route path="/call" element={authUser ? <CallPage/> : <Navigate to="/login"/>} />
+<Route path="/onboarding" element={ authUser ? <OnboardingPage/> : <Navigate to="/login"/>}/>
 
 </Routes>
 
